eadaf47f72e6840cbe56e4f55950771aa6af9c39
krlc <kirills@ieee.org>
2019-06-20 16:07:07 +0300

    Added CF header

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
             req.Header.Get("User-Agent"),
             req.Method,
             req.Host,
-            strings.Split(req.RemoteAddr, ":")[0],
+            req.Header.Get("CF-IPCountry"),
             apiString,
             time.Now().In(loc).Format("Mon Jan 2 15:04:05"),
         }
--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sudo useradd monitorservice -s /sbin/nologin -M
 sudo usermod -a -G root monitorservice
 sudo cp /home/monitor/monitorservice.service /lib/systemd/system/monitorservice.service
 sudo chmod 755 /lib/systemd/system/monitorservice.service
+sudo chmod -R 755 ./monitor_linux_x64
+sudo chmod -R 777 ./visitors.log
 
 sudo systemctl enable monitorservice.service
 sudo systemctl start monitorservice
